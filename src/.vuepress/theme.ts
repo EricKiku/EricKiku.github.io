02e133ed87f2f67f5f299e7760d3dc54d9f8613f
EricKiku <2966678301@qq.com>
2023-02-20 13:28:05 +0800

    main ten commit

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -10,7 +10,7 @@ export default hopeTheme({
     url: "https://github.com/EricKiku",
    
   },
-  iconAssets: "//at.alicdn.com/t/c/font_3891536_j5luxpdeuz.css",
+  iconAssets: "//at.alicdn.com/t/c/font_3891536_jo7r6gg586d.css",
   logo: "/roundmylogo.png",
 
   repo: "vuepress-theme-hope/vuepress-theme-hope",
